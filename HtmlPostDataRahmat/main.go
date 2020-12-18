210d03cb8110b650782fed619cd8347b8ac44616
rahmatberl <rahmatberlianda@gmail.com>
2020-12-18 16:44:11 +0700

    Tugas 3 Golang

---
+++ HtmlPostDataRahmat/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	fn "HtmlRahmat/HtmlPostDataRahmat/function"
+	"fmt"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	http.HandleFunc("/", fn.RouteIndexGet)
+	http.HandleFunc("/process", fn.RouteSubmitPost)
+
+	fmt.Println("server started at localhost:9000")
+	http.ListenAndServe(":9000", nil)
+}
